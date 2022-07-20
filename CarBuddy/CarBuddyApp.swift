479bc4cb354ee17a452503558efabf66513343be
Timo <38291523+lovetodream@users.noreply.github.com>
2022-07-20 21:52:01 +0200

    Initial Commit

---
+++ CarBuddy/CarBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarBuddyApp.swift
+//  CarBuddy
+//
+//  Created by Timo Zacherl on 20.07.22.
+//
+
+import SwiftUI
+
+@main
+struct CarBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
